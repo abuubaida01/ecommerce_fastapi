9911affbd5e48979f38b4742e26c3cc87e9612a7
abuubaida01 <abuubaida901@gmail.com>
2025-10-31 08:32:25 +0500

    completed cart-work

--- cart/router.py
+++ cart/router.py
@@ -4,6 +4,7 @@ from account.models import User
 from db.config import session
 from . import schemas as sc
 from . import services as ss
+from typing import Union
 
 router = APIRouter()
 
@@ -22,3 +23,33 @@ async def list_user_cart_items(
   user: User = Depends(get_current_user)
   ): 
   return await ss.list_user_cart(session, user.id)
+
+
+@router.patch("/increase/{product_id}", response_model=sc.CartItemOut)
+async def increase_product_quantity(
+  product_id: int, 
+  session: session, 
+  user: User = Depends(get_current_user)
+  ): 
+  return await ss.change_cart_item_qunatity_by_product(session, user.id, product_id, delta=1)
+
+
+
+@router.patch("/decrease/{product_id}", response_model=Union[sc.CartItemOut, dict])
+async def decrease_product_quantity(
+  product_id: int, 
+  session: session, 
+  user: User = Depends(get_current_user)
+  ): 
+  return await ss.change_cart_item_qunatity_by_product(session, user.id, product_id, delta=-1)
+
+
+
+@router.delete("/delete/{item_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def cart_item_delete(
+  session: session, 
+  item_id: int, 
+  user: User = Depends(get_current_user)
+  ): 
+
+  return await ss.delete_user_cart_item(session, item_id)
--- cart/services.py
+++ cart/services.py
@@ -87,3 +87,84 @@ async def list_user_cart(
       total_quantity=total_quantity,
       total_price=total_price
     )
+  
+
+async def change_cart_item_qunatity_by_product(
+    session: AsyncSession, 
+    user_id: int, 
+    product_id: int, 
+    delta: int
+  ): 
+
+  product = await session.get(Product, product_id)
+  if not product:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="product not found")
+
+  stmt = select(CartItem).where(CartItem.user_id == user_id, CartItem.product_id == product_id )
+  result = await session.execute(stmt)
+  item = result.scalar_one_or_none()
+
+  if not item: 
+    # if item does not exist, then apply below logic
+    if delta < 0: 
+      raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Item not in cart")
+
+    if product.stock_quantity <1: 
+      raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Insufficient stock")  
+    
+    # creating an object
+    item = CartItem(
+      user_id=user_id, 
+      product_id=product_id, 
+      quantity=1,
+      price=product.price
+    )
+
+    session.add(item)
+    await session.commit()
+    await session.refresh(item)
+    
+  else:
+    new_quantity = item.quantity + delta
+    if new_quantity <= 0: # if minus happened
+      await session.delete(item)
+      await session.commit()
+      return  {"message": "Item removed"}
+
+    # if we don't have products, then raise error
+    if product.stock_quantity < new_quantity: 
+      raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Insufficient stock") 
+    
+
+    item.quantity = new_quantity
+    item.price = product.price
+    await session.commit()
+    await session.refresh(item)
+  
+  # in both cases, either return new-item object, or updated item object in else
+  return sc.CartItemOut(
+    id=item.id, 
+    product_id=item.product_id,
+    product_title=product.title,
+    quantity=item.quantity,
+    user_id=item.user_id,
+    price=product.price,
+    total=round(product.price * item.quantity, 2)
+  )
+
+
+
+async def delete_user_cart_item(
+    session: AsyncSession, 
+    cart_item_id: int
+  ): 
+
+  item = await session.get(CartItem, cart_item_id)
+  if not item: 
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="item not found")
+
+  await session.delete(item)
+  await session.commit()
+  return item
+
+  
